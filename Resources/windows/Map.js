20401c41e78d22e1af3d365bcb281fd15a71551d
Mr. Evoltech <evoltech@hackbloc.org>
2012-04-24 12:40:47 -0700

    A heavy handed commit that gets everything in place for implementing GeoLoaction through OpenLayers HTML5 support.  This strips all the native geolocation code, upgrades Openlayers.

--- Resources/windows/Map.js
+++ Resources/windows/Map.js
@@ -17,8 +17,6 @@
     // Send the application settings to the js when we are loaded
     webview.addEventListener('load', function(e) {
       var settings = {};
-      settings.latitude = Ti.App.Properties.getDouble('latitude', MarchHare.settings.latitude.default_value);
-      settings.longitude = Ti.App.Properties.getDouble('longitude', MarchHare.settings.longitude.default_value);
       settings.zoom = Ti.App.Properties.getInt('zoom', MarchHare.settings.zoom.default_value);
       settings.action_domain = Ti.App.Properties.getString('action_domain', MarchHare.settings.action_domain.default_value);
       settings.poll = Ti.App.Properties.getString('poll', MarchHare.settings.poll.default_value);
