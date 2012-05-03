9f3115865bf439c88f89a33d26c6c3924a964635
Mr. Evoltech <evoltech@hackbloc.org>
2012-05-03 16:04:00 -0700

    Removing a few stale files

--- Resources/js/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  // initialize the OL object
-  var lat = 41.889818923027;
-  var lon = -87.637596505706; 
-  map = createMap('map', lat, lon, 15);
-
-  // Add some icons to it
-});
